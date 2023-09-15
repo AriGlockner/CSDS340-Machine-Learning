96337e547e79228c9a6c305182cf7c22eb664848
Ari Glockner <ari@glockners.net>
2023-09-15 17:29:35 -0400

    Solved HW2 Problem 4

--- Homework/Homework 2/lr_svm_wine.py
+++ Homework/Homework 2/lr_svm_wine.py
@@ -1,40 +1,8 @@
-from matplotlib import pyplot as plt
-from matplotlib.colors import ListedColormap
 from sklearn.datasets import load_wine
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-import numpy as np
-
-
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
-    # Markers and color maps
-    markers = ('o', 's', '^', 'v', '<')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # Plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    lab = lab.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # Plot class examples
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl,
-                    edgecolor='black')
-
-    # Highlight test examples
-    if test_idx:
-        # Plot all examples
-        X_test, y_test = X[test_idx, :], y[test_idx]
-
-        plt.scatter(X_test[:, 0], X_test[:, 1], c='', edgecolor='black', alpha=1.0, linewidth=1, marker='o',
-                    s=100, label='test set')
 
 
 # Load the wine dataset
@@ -46,29 +14,28 @@ scalar = StandardScaler()
 X = scalar.fit_transform(X)
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
-sc = StandardScaler()
-sc.fit(X_train)
-X_train_std = sc.transform(X_train)
-X_test_std = sc.transform(X_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1)
 
-# Combine the training and test sets for visualization purposes
-X_combined_std = np.vstack((X_train_std, X_test_std))
-y_combined = np.hstack((y_train, y_test))
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+c = 0.1
 
-# Fit SVM model
-svm = SVC(kernel='linear', C=1.0, random_state=1)
-svm.fit(X_train_std, y_train)
+max_accuracy = 0.0
+best_kernel = ''
+best_c = 0.0
 
-# Plot the decision regions
-plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
-plt.xlabel('Alcohol [standardized]')
-plt.ylabel('Malic Acid [standardized]')
-plt.legend(loc='upper left')
-plt.tight_layout()
-plt.show()
+for kernel in kernels:
+    for i in range(10):
+        # Fit SVM model
+        svm = SVC(kernel=kernel, C=c, random_state=1)
+        svm.fit(X_train, y_train)
 
-# Check the accuracy of the model
-predictions = svm.predict(X_test_std)
-accuracy = svm.predict(X_test_std)
-print(accuracy)
+        # Check the accuracy of the model
+        accuracy = accuracy_score(y_test, svm.predict(X_test))
+        if accuracy > max_accuracy:
+            max_accuracy = accuracy
+            best_kernel = kernel
+            best_c = c
+
+        c += 0.1
+
+print(f"The best kernel is {best_kernel} with a C value of {best_c:.1f} and an accuracy of {100*max_accuracy:.2f}%")
