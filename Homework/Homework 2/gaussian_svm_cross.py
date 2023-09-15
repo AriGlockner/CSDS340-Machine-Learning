56e7c885915816de5ff5bc4689019e9e86f7748a
Ari Glockner <ari@glockners.net>
2023-09-15 17:09:44 -0400

    Finished HW2 Problem 5

--- Homework/Homework 2/gaussian_svm_cross.py
+++ Homework/Homework 2/gaussian_svm_cross.py
@@ -1,14 +1,16 @@
+import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
+from sklearn.svm import SVC
 
-xs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-os = [(-2, 0), (0, 2), (2, 0), (0, -2)]
 
-
-def plot_data(title, show=True):
+def plot_data(d, classifier, title):
     # Plot the data
-    for i in range(4):
-        plt.plot(xs[i][0], xs[i][1], 'bx')
-        plt.plot(os[i][0], os[i][1], 'ro')
+    for xi, yi in zip(d[0], d[1]):
+        if yi == 0:
+            plt.scatter(xi[0], xi[1], c='red', marker='o', label='1')
+        else:
+            plt.scatter(xi[0], xi[1], c='blue', marker='x', label='0')
 
     # Plot settings
     plt.title(title)
@@ -17,8 +19,50 @@ def plot_data(title, show=True):
     plt.xticks([-2, -1, 0, 1, 2])
     plt.yticks([-2, -1, 0, 1, 2])
 
-    if show:
-        plt.show()
+    # Plot the decision regions
+    X = np.array(d[0])
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.02), np.arange(x2_min, x2_max, 0.02))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=ListedColormap(('red', 'blue')))
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    plt.show()
 
 
-plot_data("SVM")
+# Data
+xs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+os = [(-2, 0), (0, 2), (2, 0), (0, -2)]
+X = xs + os
+y = [1, 1, 1, 1, 0, 0, 0, 0]
+
+# Kernal types
+kernels = ['linear', 'poly', 'rbf', 'sigmoid']
+
+for kernal in kernels:
+    # Fit SVM model
+    svm = SVC(kernel=kernal, C=2.0, random_state=1)
+    svm.fit(X, y)
+
+    # Plot the decision regions
+    plot_data([X, y], svm, kernal + " SVM")
+
+'''
+a) What is the maximum training set accuracy achievable by a linear SVM?
+50%. The data is not linearly separable, so the maximum accuracy is 50%.
+
+b) Find a kernal function for an SVM that perfectly classifies the data.
+The rbf kernel function perfectly classifies the data.
+
+c) Can a Gaussian kernal SVM perfectly separate the two classes? If so, fit one to the training data and plot its
+decision region. Submit your plotted decision region.
+Yes, the Gaussian kernel SVM can perfectly separate the two classes.
+'''
+
+# Show the plotted decision region for the ideal SVM
+svm = SVC(kernel='rbf', C=2.0, random_state=1)
+svm.fit(X, y)
+plot_data([X, y], svm, "Ideal SVM")
--- Homework/Homework 2/lr_svm_wine.py
+++ Homework/Homework 2/lr_svm_wine.py
@@ -1,7 +1,41 @@
+from matplotlib import pyplot as plt
+from matplotlib.colors import ListedColormap
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 import numpy as np
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # Markers and color maps
+    markers = ('o', 's', '^', 'v', '<')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # Plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    lab = lab.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # Plot class examples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1], alpha=0.8, c=colors[idx], marker=markers[idx], label=cl,
+                    edgecolor='black')
+
+    # Highlight test examples
+    if test_idx:
+        # Plot all examples
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', edgecolor='black', alpha=1.0, linewidth=1, marker='o',
+                    s=100, label='test set')
+
 
 # Load the wine dataset
 wine = load_wine()
@@ -12,4 +46,29 @@ scalar = StandardScaler()
 X = scalar.fit_transform(X)
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y)
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# Combine the training and test sets for visualization purposes
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+# Fit SVM model
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+svm.fit(X_train_std, y_train)
+
+# Plot the decision regions
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('Alcohol [standardized]')
+plt.ylabel('Malic Acid [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# Check the accuracy of the model
+predictions = svm.predict(X_test_std)
+accuracy = svm.predict(X_test_std)
+print(accuracy)
